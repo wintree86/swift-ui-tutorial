1734e32630ba036c4914cbaff97d6811c2ffc3e9
wintree86 <mith1004@gmail.com>
2021-06-26 13:46:33 +0900

    [feature] Chaper3 Create a Category List

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -1,16 +1,24 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id:\.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         }
+        
     }
 }
 
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(ModelData())
     }
 }
