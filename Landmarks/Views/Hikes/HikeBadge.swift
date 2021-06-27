a11d5457076adb29aca8cab00b81bd7e88d92da8
wintree86 <mith1004@gmail.com>
2021-06-27 16:40:01 +0900

    [feature] Chaper3 Display a User Profile

---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
