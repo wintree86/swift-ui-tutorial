5c568cbd11fe367483d1a8f555208292b7f6a94c
wintree86 <mith1004@gmail.com>
2021-06-27 15:49:27 +0900

    [feature] Chaper3 Add Navigation Between Sections

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -13,7 +13,9 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false, content: {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }).frame(height: 185)
